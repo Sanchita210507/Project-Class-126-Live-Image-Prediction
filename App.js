3cb3c933b8a968992d7cf1fe44c5e216c49973bf
Sanchita210507 <72611487+Sanchita210507@users.noreply.github.com>
2021-12-12 20:08:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import PickImage from "./screens/Camera"
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <PickImage/>
+      
+      
+  );
+  }
+}
+
